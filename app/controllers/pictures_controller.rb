bf471be548dca96b9afed36aa9b7ac21938bb5ff
chanyaaa58 <kanegonaja@gmail.com>
2021-08-26 19:10:56 +0900

    投稿詳細ページへのリンクを作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,7 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
+  def show
+    @picture = Picture.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'pictures', to: 'pictures#index'
+  get 'pictures/:id', to: 'pictures#show'
   get 'top', to: 'home#top'
   get 'about', to: 'home#about'
-  get 'pictures', to: 'pictures#index'
+
 end
