e491693567e766e04d97efda44a43f72708815a4
chanyaaa58 <kanegonaja@gmail.com>
2021-08-30 17:46:38 +0900

    マイグレーションファイルの矛盾を解消

--- db/migrate/20210826075711_create_pictures.rb
+++ db/migrate/20210826075711_create_pictures.rb
@@ -2,7 +2,7 @@ class CreatePictures < ActiveRecord::Migration[5.2]
   def change
     create_table :pictures do |t|
       t.text :content
-
+      t.string :image
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_075711) do
+ActiveRecord::Schema.define(version: 2021_08_30_052343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pictures", force: :cascade do |t|
     t.text "content"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
