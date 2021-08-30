c9d328c8d97f86210a4b6895e0780224f4a72b92
chanyaaa58 <kanegonaja@gmail.com>
2021-08-30 17:48:00 +0900

    ルーティング修正、Userのコントローラー完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'pictures', to: 'pictures#index'
-  get 'pictures/new', to: 'pictures#new'
-  get 'pictures/:id', to: 'pictures#show'
-  post 'pictures/create', to: 'pictures#create'
-  get 'pictures/:id/edit', to: 'pictures#edit'
-  post 'pictures/:id/update', to: 'pictures#update'
-  post 'pictures/:id/destroy', to: 'pictures#destroy'
-
+  resources :pictures
+  resources :users
   get 'top', to: 'home#top'
   get 'about', to: 'home#about'
 end
