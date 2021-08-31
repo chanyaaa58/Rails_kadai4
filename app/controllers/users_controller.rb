f60a255c9a393dee424114429d0718b641a65ce3
chanyaaa58 <kanegonaja@gmail.com>
2021-08-31 10:57:39 +0900

    ユーザー登録が問題なくできるように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-  end
+  # def index
+  #   @users = User.all
+  # end
 
   def new
     @user = User.new
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   
   private
     def user_params
-      params.require(:user).permit(:neme, :email, :password,
+      params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
 end
