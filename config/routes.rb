5300c302570c1cf7450fa599ac3d626bc7b14e31
chanyaaa58 <kanegonaja@gmail.com>
2021-09-01 18:11:58 +0900

    お気に入り機能実装、お気に入り一覧画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :sessions
   resources :pictures
   resources :users
+  resources :favorites, only: [:create, :destroy, :index]
   get 'top', to: 'home#top'
   get 'about', to: 'home#about'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
